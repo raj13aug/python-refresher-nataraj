7be7191c21b1ca72ac733d9667ea317fc364cc7e
Nataraj.R <raj13aug@gmail.com>
2021-05-09 17:09:15 +0530

    user & first-python programming

---
+++ user-input.py
@@ -0,0 +1,6 @@
+size_input = input("How big is your house ( in squere feet")
+square_feet = int(size_input)
+square_meter = square_feet / 10.8
+print(f"{square_feet} square feet is {square_meter: .2f} square meter")
+
+# https://blog.teclado.com/python-formatting-numbers-for-printing/
