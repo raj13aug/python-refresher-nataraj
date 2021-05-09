47dd07d270db4635022d371601817fe6896fb189
Nataraj.R <raj13aug@gmail.com>
2021-05-09 16:42:08 +0530

    string

---
+++ String-formatting.py
@@ -0,0 +1,17 @@
+name = "Raj"
+
+output = f"Hello!, {name}"
+print(output)
+
+name = "Mano"
+
+value = "Hello, {}"
+print(value.format(name))
+
+name = "Nataraj"
+course = "Python"
+letter = "Heloo {1} is am back to {0}"
+
+formatted = letter.format(course, name)
+
+print(formatted)
