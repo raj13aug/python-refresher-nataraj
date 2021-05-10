0b1257ccf00ca262559c554613f5f103bfbd995e
Nataraj Ramamoorthy <raj13aug@yahoo.in>
2021-05-10 21:30:42 +0530

    Create in_word.py

---
+++ in_word.py
@@ -0,0 +1,7 @@
+#"In"function check the one of the members inthis list. 
+#The in keyword works in sequences like lists,tuples,sets also works in string
+
+movies_watched = {"The matrix", "Green Book", "Her"}
+user_movie = input("Enter something you have watched recently: ")
+
+print(user_movie in movies_watched)
