003db106cf211c7856f3a9b803c818ed57cdee89
Nataraj Ramamoorthy <raj13aug@yahoo.in>
2021-05-10 19:44:54 +0530

    Create Boolean.py

---
+++ Boolean.py
@@ -0,0 +1,10 @@
+#Boolean is a value that can be either true or false.Booleans are created by using comparsions.
+
+#IS is check two elements are exactly  the same thing are not
+#python goes and creates some space in its memory for this new list. so two list created the different memory location.
+
+friends = ["rolf", "Bob"]
+abroad =  ["rolf", "Bob"]
+print(friends == abroad)
+print(friends is abroad)
+
