b15d5db687bd9ba781ab0ddc74644d60585e2778
Nataraj Ramamoorthy <raj13aug@yahoo.in>
2021-05-14 15:12:43 +0530

    Update function.py

--- function.py
+++ function.py
@@ -2,3 +2,15 @@ def hello():
   print("Hello")
 
 hello()
+
+
+print("Welcome to the age in second program")
+
+def user_age_in_seconds():
+  user_age = int(input("Enter your age: "))
+  age_seconds = user_age * 365 * 24 * 60 * 60
+  print(f"your age in seconds is {age_seconds}")
+
+user_age_in_seconds()
+
+print("Goodbye")
