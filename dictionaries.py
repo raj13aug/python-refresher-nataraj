86d65b981ec2b814679e89465e4613d68786ae22
Nataraj Ramamoorthy <raj13aug@yahoo.in>
2021-05-14 12:49:53 +0530

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -5,3 +5,32 @@ friends = [
 ]
 
 print(friends[0]["name"])
+
+
+student_data = {"Ram": 50, "vino": 98, "shamantha": 89}
+
+for student in student_data:
+  print(student, ": ", student_data[student])
+  print(f"{student} : {student_data[student]}")
+
+
+#items() --> studendance.items() give you two values for each student that you can get as two separate variables here in python.
+
+student_data = {"Ram": 50, "vino": 98, "shamantha": 89}
+
+for key,value in student_data.items():
+  print(f"items value : {key}:{value}")
+
+#for loop using dict
+student_attendance = {"Rolf": 96, "Bob": 80, "Anne": 100}
+
+if "Bob" in student_attendance:
+  print(f"Bob: {student_attendance['Bob']}")
+else:
+  print("Bob is not a student in this class")
+
+#Average of value:
+
+average_value = student_attendance.values()
+total = sum(average_value) / len(average_value)
+print(total)
